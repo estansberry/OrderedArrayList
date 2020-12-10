bfb8e6127b59ac441d78eb3c4b5c08985a1f83d8
Elizabeth Stansberry <estansberry20@stuy.edu>
2020-12-10 11:16:47 -0700

    Changes

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,2 +1,25 @@
 import java.util.ArrayList;
-NoNullArrayList<T> extends ArrayList<T>
+NoNullArrayList<T> extends ArrayList<T> {
+  public void add(Object a){
+    if(a == null){
+      throw new IllegalArgumentException();
+    }else{
+      T.add(a);
+    }
+  }
+  public void add(int index, Object a){
+    if(a == null){
+      throw new IllegalArgumentException();
+    }else{
+      T.add(index, a);
+    }
+  }
+
+  public void set(int index, Object a){
+    if(a == null){
+      throw new IllegalArgumentException();
+    }else{
+      T.set(index, a);
+    }
+  }
+}
