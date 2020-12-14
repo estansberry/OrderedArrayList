049f7b9a85acf1ceb2818b58e5509049cd256e73
Elizabeth Stansberry <estansberry20@stuy.edu>
2020-12-14 08:50:42 -0700

    Fixes after testing

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -2,16 +2,19 @@ import java.util.ArrayList;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
   public OrderedArrayList(){
     super();
-    ArrayList<T> newarr = new ArrayList<T>();
+    NoNullArrayList<T> newarr = new NoNullArrayList<T>();
   }
   public OrderedArrayList(int size){
     super();
-    ArrayList<T> newarr = new ArrayList<T>(size);
+    NoNullArrayList<T> newarr = new NoNullArrayList<T>(size);
   }
   public boolean add(T a){
     for(int i = 0; i < size(); i ++){
       if(a.compareTo(super.get(i)) >= 0){
-        super.add(a);
+        super.add(i, a);
+        break;
+      }else{
+        super.add(size() - 1, a);
       }
     }return(true);
   }
@@ -19,9 +22,11 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     add(a);
   }
   public T set(int index, T a){
-    T x = super.get(index);
-    super.remove(index);
+    T y = null;
+    if(super.size() != 0){
+      y = super.get(index);
+    }super.remove(index);
     add(a);
-    return(x);
+    return(y);
   }
 }
